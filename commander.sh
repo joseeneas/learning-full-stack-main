80bab747e7f2a5045099ccae33b09eedd032b75e
Jose Eneas da Silva Maria <joseeneas@me.com>
2025-11-22 12:46:13 -0500

    Docker Starting Script

---
+++ commander.sh
@@ -0,0 +1,7 @@
+ocker network create fullnet || true
+docker network connect fullnet test-postgres
+docker run --name fullstack --network fullnet -p 8080:8080 \
+  -e SPRING_DATASOURCE_URL=jdbc:postgresql://test-postgres:5432/syscomz \
+  -e SPRING_DATASOURCE_USERNAME=eneas \
+  -e SPRING_DATASOURCE_PASSWORD=top2gun6 \
+  springboot-react-fullstack:local
